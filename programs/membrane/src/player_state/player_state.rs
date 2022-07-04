e0756ffcd1237071992ef03bd6037cd4d57e6f40
yerikvaliyev <yerikvaliyev@gmail.com>
2022-07-04 20:36:12 +0600

    create_player fix attempt

--- programs/membrane/src/player_state/player_state.rs
+++ programs/membrane/src/player_state/player_state.rs
@@ -7,11 +7,13 @@ pub fn create_player(ctx: Context<InitializePlayer>, rating: Option<i64>) -> Res
     player.claimable = 0;
     player.nft_counter = 1; //account is created when user buys their first nft
     player.user = *user.key;
-    player.bump = *ctx.bumps.get("player").unwrap();
+
     match rating {
         Some(x) => player.rating = Some(x),
         None => player.rating = Some(0),
     };
+
+    player.bump = *ctx.bumps.get("player").unwrap();
     
     Ok(())
 }
@@ -24,7 +26,7 @@ pub fn update_player(ctx: Context<UpdatePlayer>) -> Result<()> {
 
 #[derive(Accounts)]
 pub struct InitializePlayer<'info> {
-    #[account(init, payer = user, space = constants::MAX_PLAYER_SIZE, seeds = [b"player_account".as_ref(), user.key().as_ref()], bump)]
+    #[account(init, payer = user, space = constants::MAX_PLAYER_SIZE, seeds = [b"player", user.key().as_ref()], bump)]
     pub player: Account<'info, Player>,
     #[account(mut)]
     pub user: Signer<'info>,
@@ -34,7 +36,7 @@ pub struct InitializePlayer<'info> {
 #[derive(Accounts)]
 #[instruction(bump: u8)]
 pub struct UpdatePlayer<'info> {
-    #[account(mut, has_one = user, seeds = [b"player_account".as_ref(), user.key().as_ref()], bump = bump)]
+    #[account(mut, has_one = user, seeds = [b"player", user.key().as_ref()], bump = bump)]
     pub player: Account<'info, Player>,
     #[account(mut)]
     pub user: Signer<'info>,
