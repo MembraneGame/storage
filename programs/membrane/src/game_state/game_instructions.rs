f9a3d06f29973e30bb5dcb106fc351843ce3864b
yerikvaliyev <yerikvaliyev@gmail.com>
2022-07-28 14:48:20 +0600

    debug

--- programs/membrane/src/game_state/game_instructions.rs
+++ programs/membrane/src/game_state/game_instructions.rs
@@ -106,10 +106,11 @@ pub fn calculate_reward(ctx: Context<CalculateReward>, placement: u64, kills: u6
     msg!("Stat: {:?}", stat);
     let stats = &mut ctx.accounts.players_stats.load_mut()?;
     msg!("Before: {}", stats.counter);
+    msg!("PlayersStats before: {:?}", stats.players);
     // let counter = stats.counter as usize; //value declared explicitly to avoid null pointer
     stats.append(stat);
     msg!("After: {}", stats.counter);
-    msg!("PlayersStats: {:?}", stats.players);
+    msg!("PlayersStats after: {:?}", stats.players);
     // stats.players[counter] = stat;
     // stats.counter = stats.counter + 1;
 
